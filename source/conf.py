a6a52c524e117941a2ee5ee5eb7caa3fe43b04da
Believer <longqh3@outlook.com>
2020-12-22 16:56:50 +0800

    Update conf.py
    solve two dashes' display issue

--- source/conf.py
+++ source/conf.py
@@ -43,6 +43,9 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# 2020.12.22
+# Solve two dashes' display issue
+smartquotes=False
 
 # -- Options for HTML output -------------------------------------------------
 
